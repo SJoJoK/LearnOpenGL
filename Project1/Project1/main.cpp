0108119a9c217fbcdfdf75ff2e44d352045bffee
SJoJoK <450986321@qq.com>
2021-09-17 17:34:19 +0800

    尝试纹理但出错

--- Project1/Project1/main.cpp
+++ Project1/Project1/main.cpp
@@ -2,22 +2,7 @@
 #include <GLFW/glfw3.h>
 #include "shader.h"
 #include <iostream>
-const char* vertexShaderSource = "#version 330 core\n"
-"layout (location = 0) in vec3 aPos;\n"
-"layout (location = 1) in vec3 aColor;\n"
-"out vec3 ourColor;\n"
-"void main()\n"
-"{\n"
-"   gl_Position = vec4(aPos.x, aPos.y, aPos.z, 1.0);\n"
-"   ourColor = aColor;\n"
-"}\0";
-const char* fragmentShaderSource = "#version 330 core\n"
-"out vec4 FragColor;\n"
-"in vec3 ourColor;\n"
-"void main()\n"
-"{\n"
-"   FragColor = vec4(ourColor, 1.0);\n"
-"}\0";
+#include "stb_image.h"
 void framebuffer_size_callback(GLFWwindow* window, int width, int height)
 {
     glViewport(0, 0, width, height);
@@ -49,19 +34,22 @@ int main()
     glViewport(0, 0, 800, 600);
     glfwSetFramebufferSizeCallback(window, framebuffer_size_callback);
     float vertices[] = {
-        // 位置              // 颜色
-         0.5f, -0.5f, 0.0f,  1.0f, 0.0f, 0.0f,   // 右下
-        -0.5f, -0.5f, 0.0f,  0.0f, 1.0f, 0.0f,   // 左下
-         0.0f,  0.5f, 0.0f,  0.0f, 0.0f, 1.0f    // 顶部
+        //     ---- 位置 ----       ---- 颜色 ----     - 纹理坐标 -
+             0.5f,  0.5f, 0.0f,   1.0f, 0.0f, 0.0f,   1.0f, 1.0f,   // 右上
+             0.5f, -0.5f, 0.0f,   0.0f, 1.0f, 0.0f,   1.0f, 0.0f,   // 右下
+            -0.5f, -0.5f, 0.0f,   0.0f, 0.0f, 1.0f,   0.0f, 0.0f,   // 左下
+            -0.5f,  0.5f, 0.0f,   1.0f, 1.0f, 0.0f,   0.0f, 1.0f    // 左上
     };
-
-    //unsigned int indices[] = { // 注意索引从0开始! 
-    //    0, 1, 3, // 第一个三角形
-    //    1, 2, 3  // 第二个三角形
-    //};
-    int  success_shader;
-    int  success_link;
-    char infoLog[512];
+    float texCoords[] = {
+    0.0f, 0.0f, // 左下角
+    1.0f, 0.0f, // 右下角
+    0.5f, 1.0f // 上中
+    };
+    unsigned int indices[] = { // 注意索引从0开始! 
+        0, 1, 3, // 第一个三角形
+        1, 2, 3  // 第二个三角形
+    };
+    Shader ourShader("vshader.glsl", "fshader.glsl");
 
     int nrAttributes;
     glGetIntegerv(GL_MAX_VERTEX_ATTRIBS, &nrAttributes);
@@ -79,20 +67,43 @@ int main()
     unsigned int EBO;
     glGenBuffers(1, &EBO);
 
+    //Texture
+    unsigned int texture;
+    glGenTextures(1, &texture);
+    glBindTexture(GL_TEXTURE_2D, texture);
+    // 为当前绑定的纹理对象设置环绕、过滤方式
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR_MIPMAP_LINEAR);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+    // 加载并生成纹理
+    int width, height, nrChannels;
+    unsigned char* data = stbi_load("container.jpg", &width, &height, &nrChannels, 0);
+    if (data)
+    {
+        glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB, width, height, 0, GL_RGB, GL_UNSIGNED_BYTE, data);
+        glGenerateMipmap(GL_TEXTURE_2D);
+    }
+    else
+    {
+        std::cout << "Failed to load texture" << std::endl;
+    }
+    stbi_image_free(data);
+    
     // 1. 绑定VAO
     glBindVertexArray(VAO);
     // 2. 把顶点数组复制到缓冲中供OpenGL使用
     glBindBuffer(GL_ARRAY_BUFFER, VBO);
     glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
     // 3. 复制我们的索引数组到一个索引缓冲中，供OpenGL使用
-    //glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);
-    //glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices, GL_STATIC_DRAW);
+    glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);
+    glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices, GL_STATIC_DRAW);
     // 4. 设置顶点属性指针
     glVertexAttribPointer(0,//要配置的顶点属性
         3,//顶点属性的大小，一个有几个值
         GL_FLOAT,//数据类型
         GL_FALSE,//是否归一
-        6 * sizeof(float),//步长，连续的顶点属性组之间的间隔
+        8 * sizeof(float),//步长，连续的顶点属性组之间的间隔
         (void*)0//数据距离缓冲起始位置的偏移量
     );
     glEnableVertexAttribArray(0);//启动顶点属性
@@ -100,12 +111,18 @@ int main()
         3,//顶点属性的大小，一个有几个值
         GL_FLOAT,//数据类型
         GL_FALSE,//是否归一
-        6 * sizeof(float),//步长，连续的顶点属性组之间的间隔
+        8 * sizeof(float),//步长，连续的顶点属性组之间的间隔
         (void*)(3*sizeof(float))//数据距离缓冲起始位置的偏移量
     );
     glEnableVertexAttribArray(1);//启动顶点属性
-
-    Shader ourShader("vshader.vs", "fshader.fs");
+    glVertexAttribPointer(2,//要配置的顶点属性
+        2,//顶点属性的大小，一个有几个值
+        GL_FLOAT,//数据类型
+        GL_FALSE,//是否归一
+        8 * sizeof(float),//步长，连续的顶点属性组之间的间隔
+        (void*)(6 * sizeof(float))//数据距离缓冲起始位置的偏移量
+    );
+    glEnableVertexAttribArray(1);//启动顶点属性
 
     while (!glfwWindowShouldClose(window))
     {
@@ -121,7 +138,7 @@ int main()
         ourShader.use();
 
         glBindVertexArray(VAO);
-        glDrawArrays(GL_TRIANGLES, 0, 3);
+        glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
         glBindVertexArray(0);
         //PollEvents and Swap
         glfwPollEvents();
