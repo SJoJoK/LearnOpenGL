fd552af680ac8ffe0a5d124d8e92d3709cfcc210
SJoJoK <450986321@qq.com>
2021-09-20 15:27:58 +0800

    算了还是先做PBR吧

--- Project1/Project1/main.cpp
+++ Project1/Project1/main.cpp
@@ -216,7 +216,8 @@ int main()
     GLuint depthMapFBO;
     glGenFramebuffers(1, &depthMapFBO);
     glBindFramebuffer(GL_FRAMEBUFFER, depthMapFBO);
-    GLuint depthMap;
+    GLuint depthMap, depthMap1;
+
     glGenTextures(1, &depthMap);
     glBindTexture(GL_TEXTURE_2D, depthMap);
     glTexImage2D(GL_TEXTURE_2D, 0, GL_DEPTH_COMPONENT,
@@ -225,6 +226,7 @@ int main()
     glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_NEAREST);
     glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT);
     glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT);
+
     glFramebufferTexture2D(GL_FRAMEBUFFER, GL_DEPTH_ATTACHMENT, GL_TEXTURE_2D, depthMap, 0);
     glDrawBuffer(GL_NONE);
     glReadBuffer(GL_NONE);
@@ -288,6 +290,7 @@ int main()
                                    -1 * plight_arr.bulb_radius * sin(glm::radians(plight_arr.bulb_degree)));
 
         // 1. 首选渲染深度贴图
+        glBindTexture(GL_TEXTURE_2D, depthMap);
         glViewport(0, 0, SHADOW_WIDTH, SHADOW_HEIGHT);
         glBindFramebuffer(GL_FRAMEBUFFER, depthMapFBO);
         glClear(GL_DEPTH_BUFFER_BIT);
